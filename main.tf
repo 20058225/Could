82d5c26353972eac330b73ab194bea4f5f7d1791
20058225 <20058225@mydbs.ie>
2024-10-22 18:29:58 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,21 +1,67 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"  
+    }
+  }
+
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "a77c589e-092e-413f-9643-e91ac54cdb6a"
+}
+
+data "azurerm_resource_group" "example" {
+  name = "B9IS121_group"
+}
+
+resource "azurerm_virtual_network" "vnet" {
+  name                = "B9IS121-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = "westeurope"
+  resource_group_name = "B9IS121_group"
+}
+
+resource "azurerm_subnet" "subnet" {
+  name                 = "B9IS121-subnet"
+  resource_group_name  = "B9IS121_group"
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_network_interface" "nic" {
+  name                = "B9IS121-nic"
+  location            = "westeurope"
+  resource_group_name = data.azurerm_resource_group.example.name
+
+  ip_configuration {
+    name                          = "B9IS121-ipconfig"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "vm" {
-  name                = "B9IS121"  # Ensure this matches
-  resource_group_name = "B9IS121_group"  # Ensure this matches
-  location            = "westeurope"  # Ensure this matches
-  size                = "Standard_B1s"  # Ensure this matches
-  admin_username      = "brenda"  # Ensure this matches
+  name                = "B9IS121"
+  resource_group_name = "B9IS121_group"
+  location            = "westeurope"
+  size                = "Standard_B1s"
+  admin_username      = "brenda"
 
   admin_ssh_key {
     username   = "brenda"
     public_key = file("/home/brenda/.ssh/id_rsa.pub")
   }
 
-  network_interface_ids = [azurerm_network_interface.nic.id]  # Ensure NIC is configured correctly
+  network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
     caching              = "ReadWrite"
     storage_account_type = "Premium_LRS"
-    disk_size_gb         = 30  # Ensure this matches
+    disk_size_gb         = 30
   }
 
   source_image_reference {
@@ -24,4 +70,12 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "server"
     version   = "latest"
   }
+}
+
+output "resource_group_id" {
+  value = data.azurerm_resource_group.example.id
+}
+
+output "vm_id" {
+  value = azurerm_linux_virtual_machine.vm.id
 }
