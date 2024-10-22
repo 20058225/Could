6ff9776d8f3fe3d75d763c8ce948f50ace03a98f
20058225 <20058225@mydbs.ie>
2024-10-22 17:38:48 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,58 +11,57 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "a77c589e-092e-413f-9643-e91ac54cdb6a"  # Replace with your actual subscription ID
+  subscription_id = "a77c589e-092e-413f-9643-e91ac54cdb6a"
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "B9IS121_GROUP"
-  location = "westeurope"
+# Data block to retrieve an existing resource group
+data "azurerm_resource_group" "example" {
+  name = "B9IS121_GROUP"
 }
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "B9IS121-vnet"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  address_space       = ["10.1.0.0/16"]
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 }
-
 resource "azurerm_subnet" "subnet" {
   name                 = "B9IS121-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.example.name
   virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
+  address_prefixes     = ["10.1.1.0/24"]
 }
 
 resource "azurerm_network_interface" "nic" {
   name                = "B9IS121-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.azurerm_resource_group.example.location
+  resource_group_name = data.azurerm_resource_group.example.name
 
   ip_configuration {
     name                          = "B9IS121-ipconfig"
-    subnet_id                    = azurerm_subnet.subnet.id
+    subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Dynamic"
-  }
+ }
 }
 
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "B9IS121"
-  resource_group_name = azurerm_resource_group.rg.name
-  location            = azurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.example.name
+  location            = data.azurerm_resource_group.example.location
   size                = "Standard_B1s"
   admin_username      = "brenda"
 
   admin_ssh_key {
     username   = "brenda"
-    public_key = file("/home/brenda/.ssh/id_rsa.pub")  # Ensure this path is correct
+    public_key = file("/home/brenda/.ssh/id_rsa.pub")
   }
 
   network_interface_ids = [azurerm_network_interface.nic.id]
 
   os_disk {
     caching              = "ReadWrite"
-    storage_account_type = "Premium_LRS"  # Required argument
-    disk_size_gb        = 30                # Specify disk size
+    storage_account_type = "Premium_LRS"
+    disk_size_gb         = 30
   }
 
   source_image_reference {
@@ -71,12 +70,11 @@ resource "azurerm_linux_virtual_machine" "vm" {
     sku       = "server"
     version   = "latest"
   }
-
-  # Additional configurations can be added here if needed
 }
 
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
+# Output the ID of the existing resource group
+output "resource_group_id" {
+  value = data.azurerm_resource_group.example.id
 }
 
 output "vm_id" {
