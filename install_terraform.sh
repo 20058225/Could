f016560b3434c70d8a8582d98426d8356e12f23c
Brenda Lopes <20058225@mydbs.ie>
2024-10-24 17:11:33 -0300

    Update install_terraform.sh

--- install_terraform.sh
+++ install_terraform.sh
@@ -5,27 +5,22 @@ echo "Installing necessary dependencies..."
 sudo apt-get update
 sudo apt-get install -y gnupg software-properties-common curl unzip
 
-# Get the latest version URL dynamically
-LATEST_VERSION=$(curl -sL https://releases.hashicorp.com/terraform/ | grep -oP 'terraform/[\d.]+/' | head -1 | sed 's#/##g')
-echo "Latest version: $LATEST_VERSION"
+# Add HashiCorp GPG key and repo
+echo "Adding HashiCorp GPG key and repository..."
+curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
+sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
 
-# Download Terraform
-echo "Downloading Terraform..."
-curl -O https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+# Update package lists
+echo "Updating package lists..."
+sudo apt-get update
 
-# Unzip the Terraform package
-echo "Unzipping Terraform..."
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
-# Move Terraform to /usr/local/bin
+# Install Terraform using apt-get
 echo "Installing Terraform..."
-sudo mv terraform /usr/local/bin/
+sudo apt-get install -y terraform
 
-# Verify installation
+# Verify Terraform installation
 echo "Verifying the Terraform installation..."
 terraform --version
 
-# Clean up
-echo "Cleaning up..."
-rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+# Installation complete
 echo "Terraform installation complete."
