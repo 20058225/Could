8d5a15ca4e8cdf27224009cd895831481a69ee4c
Brenda Lopes <20058225@mydbs.ie>
2024-10-24 15:49:50 -0300

    Update install_terraform.sh

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
-sudo apt-get update && sudo apt-get install -y gnupg software-properties-common curl
-curl -fsSL https://apt.releases.hashicorp.com/gpg | sudo apt-key add -
-sudo apt-add-repository "deb [arch=amd64] https://apt.releases.hashicorp.com $(lsb_release -cs) main"
-sudo apt-get update && sudo apt-get install terraform
+
+# Script to install Terraform
+
+TERRAFORM_VERSION="1.5.6"
+
+# Download Terraform
+echo "Downloading Terraform..."
+wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Unzip the Terraform package
+echo "Unzipping Terraform..."
+unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+# Move the Terraform binary to /usr/local/bin
+echo "Installing Terraform..."
+sudo mv terraform /usr/local/bin/
+
+# Verify the installation
+echo "Verifying the Terraform installation..."
+terraform --version
+
+# Clean up
+echo "Cleaning up..."
+rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+
+echo "Terraform installation complete."
