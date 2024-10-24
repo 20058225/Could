649d6f588529fd3eb2b198aa059a165f90a559e5
Brenda Lopes <20058225@mydbs.ie>
2024-10-24 18:07:42 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,41 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+  required_version = ">= 1.0.0"
+}
+
+provider "azurerm" {
+  features {}
+  subscription_id = "a77c589e-092e-413f-9643-e91ac54cdb6a"
+}
+
+# Declare Resource Group
+resource "azurerm_resource_group" "rg" {
+  name     = "B9IS121-rg"
+  location = "westeurope"
+}
+
+# Declare Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "B9IS121-vnet"
+  address_space       = ["10.1.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Declare Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "B9IS121-subnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.1.0.0/24"]
+}
+
+# Declare Network Security Group with Fixed Protocol
 resource "azurerm_network_security_group" "nsg" {
   name                = "B9IS121-nsg"
   location            = azurerm_resource_group.rg.location
@@ -8,7 +46,7 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 300
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"  # Fixed: Changed from TPC to Tcp
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
@@ -20,7 +58,7 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 320
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"  # Fixed: Changed from TPC to Tcp
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "80"
     source_address_prefix      = "*"
@@ -32,7 +70,7 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 340
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "Tcp"  # Fixed: Changed from TPC to Tcp
+    protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefix      = "*"
@@ -40,6 +78,30 @@ resource "azurerm_network_security_group" "nsg" {
   }
 }
 
+# Declare Public IP
+resource "azurerm_public_ip" "public_ip" {
+  name                = "B9IS121-public_ip"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  allocation_method   = "Static"
+  sku 				  = "Standard"
+}
+
+# Declare Network Interface
+resource "azurerm_network_interface" "nic" {
+  name                = "B9IS121-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "B9IS121-nic-config"
+    subnet_id                     = azurerm_subnet.subnet.id
+    private_ip_address_allocation = "Static"
+    public_ip_address_id          = azurerm_public_ip.public_ip.id
+  }
+}
+
+# Declare Virtual Machine
 resource "azurerm_linux_virtual_machine" "vm" {
   name                = "B9IS121-vm"
   location            = azurerm_resource_group.rg.location
@@ -49,7 +111,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
   ]
 
   size               = "Standard_B1s"
-  admin_username     = "brenda"
+  admin_username     = "useradmin"
   admin_password     = "P@ssw0rd123!"
 
   os_disk {
@@ -66,4 +128,10 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   computer_name  = "B9IS121host"
   disable_password_authentication = false
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
