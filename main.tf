4cdf577de3962e705019851961c1618d185a9a45
Brenda Lopes <20058225@mydbs.ie>
2024-10-24 18:28:38 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -97,6 +97,7 @@ resource "azurerm_network_interface" "nic" {
     name                          = "B9IS121-nic-config"
     subnet_id                     = azurerm_subnet.subnet.id
     private_ip_address_allocation = "Static"
+    private_ip_address            = "10.1.0.4"  
     public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
