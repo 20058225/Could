b8df5f669618af2714d57ee760dc90760e1fd9ea
Brenda Lopes <20058225@mydbs.ie>
2024-10-24 17:59:27 -0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,37 +1,3 @@
-terraform {
-  required_providers {
-    azurerm = {
-      source  = "hashicorp/azurerm"
-      version = "~> 4.0"  
-    }
-  }
-  required_version = ">= 1.0.0"
-}
-
-provider "azurerm" {
-  features {}
-  subscription_id = "a77c589e-092e-413f-9643-e91ac54cdb6a"  
-}
-
-resource "azurerm_resource_group" "rg" {
-  name     = "B9IS121-rg"
-  location = "westeurope"
-}
-
-resource "azurerm_virtual_network" "vnet" {
-  name                = "B9IS121-vnet"
-  address_space       = ["10.1.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-resource "azurerm_subnet" "subnet" {
-  name                 = "B9IS121-subnet"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.1.0.0/24"]
-}
-
 resource "azurerm_network_security_group" "nsg" {
   name                = "B9IS121-nsg"
   location            = azurerm_resource_group.rg.location
@@ -42,55 +8,35 @@ resource "azurerm_network_security_group" "nsg" {
     priority                   = 300
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TPC"
+    protocol                   = "Tcp"  # Fixed: Changed from TPC to Tcp
     source_port_range          = "*"
     destination_port_range     = "22"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
 
- security_rule {
+  security_rule {
     name                       = "HTTP"
     priority                   = 320
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TPC"
+    protocol                   = "Tcp"  # Fixed: Changed from TPC to Tcp
     source_port_range          = "*"
     destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
-   security_rule {
+
+  security_rule {
     name                       = "HTTPS"
     priority                   = 340
     direction                  = "Inbound"
     access                     = "Allow"
-    protocol                   = "TPC"
+    protocol                   = "Tcp"  # Fixed: Changed from TPC to Tcp
     source_port_range          = "*"
     destination_port_range     = "443"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
-  }
-}
-
-resource "azurerm_public_ip" "public_ip" {
-  name                = "B9IS121-public_ip"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-  allocation_method   = "Static"
-  sku 				  = "Standard"
-}
-
-resource "azurerm_network_interface" "nic" {
-  name                = "B9IS121-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-
- ip_configuration {
-    name                          = "B9IS121-nic-config"
-    subnet_id                     = azurerm_subnet.subnet.id
-    private_ip_address_allocation = "Static"
-    public_ip_address_id          = azurerm_public_ip.public_ip.id
   }
 }
 
@@ -104,7 +50,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   size               = "Standard_B1s"
   admin_username     = "brenda"
-  admin_password     = "123456"
+  admin_password     = "P@ssw0rd123!"
 
   os_disk {
     caching              = "ReadWrite"
@@ -121,9 +67,3 @@ resource "azurerm_linux_virtual_machine" "vm" {
   computer_name  = "B9IS121host"
   disable_password_authentication = false
 }
-
-resource "azurerm_subnet_network_security_group_association" "nsg_assoc" {
-  subnet_id                 = azurerm_subnet.subnet.id
-  network_security_group_id = azurerm_network_security_group.nsg.id
-}
-	
