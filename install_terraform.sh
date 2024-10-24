6a965e61e8b4eee716b20d629ad86ee46eb01300
Brenda Lopes <20058225@mydbs.ie>
2024-10-24 16:02:36 -0300

    Update install_terraform.sh

--- install_terraform.sh
+++ install_terraform.sh
@@ -1,27 +1,31 @@
 #!/bin/bash
 
-# Script to install Terraform
+# Install dependencies
+echo "Installing necessary dependencies..."
+sudo apt-get update
+sudo apt-get install -y gnupg software-properties-common curl unzip
 
-TERRAFORM_VERSION="1.5.6"
+# Get the latest version URL dynamically
+LATEST_VERSION=$(curl -sL https://releases.hashicorp.com/terraform/ | grep -oP 'terraform/[\d.]+/' | head -1 | sed 's#/##g')
+echo "Latest version: $LATEST_VERSION"
 
 # Download Terraform
 echo "Downloading Terraform..."
-wget https://releases.hashicorp.com/terraform/${TERRAFORM_VERSION}/terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+curl -O https://releases.hashicorp.com/terraform/${LATEST_VERSION}/terraform_${LATEST_VERSION}_linux_amd64.zip
 
 # Unzip the Terraform package
 echo "Unzipping Terraform..."
-unzip terraform_${TERRAFORM_VERSION}_linux_amd64.zip
+unzip terraform_${LATEST_VERSION}_linux_amd64.zip
 
-# Move the Terraform binary to /usr/local/bin
+# Move Terraform to /usr/local/bin
 echo "Installing Terraform..."
 sudo mv terraform /usr/local/bin/
 
-# Verify the installation
+# Verify installation
 echo "Verifying the Terraform installation..."
 terraform --version
 
 # Clean up
 echo "Cleaning up..."
-rm terraform_${TERRAFORM_VERSION}_linux_amd64.zip
-
+rm terraform_${LATEST_VERSION}_linux_amd64.zip
 echo "Terraform installation complete."
