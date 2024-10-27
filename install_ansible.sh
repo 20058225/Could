727f53bf6f886f3b043e89d21b41250b2e32b6b1
Brenda Lopes <20058225@mydbs.ie>
2024-10-27 11:33:34 -0300

    Update install_ansible.sh

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,22 +3,22 @@ echo "System Preparation in progress..."
   sudo apt update
   sudo apt install software-properties-common
   sudo apt-add-repository --yes --update ppa:ansible/ansible
-  sudo apt install ansible
+  sudo apt install ansible -y
 
 # Installing pip
 echo "Installing pip in progress..."
   curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
   python3 get-pip.py --user
-  sudo apt-get install python3-pip
+  sudo apt-get install python3-pip -y
     #Shows the version 
     python3 -m pip -V
 
 # Setting Up the Python Virtual Environment
 echo "Setting Up the Python Virtual Environment in progress..."
-  sudo apt install python3-venv
+  sudo apt install python3-venv -y
   python3 -m venv myenv
   source myenv/bin/activate
-  pip install ansible
+  pip install ansible -y
   # Creating and activating a virtual environment keeps the Ansible installation isolated from the system Python, reducing potential conflicts.
 
 # Testing Ansible with an Inventory and Playbook
