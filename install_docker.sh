dd26daad9597786670656c73458669c8cd7b9299
20058225 <20058225@mydbs.ie>
2024-11-07 17:02:35 -0300

    fix files

--- install_docker.sh
+++ install_docker.sh
@@ -9,18 +9,18 @@ cd app
 
 # Installing npm and expres
     echo "Installing npm and express"
-sudo apt install npm
+sudo apt install npm -y
 npm init -y
-npm install express
+npm install express -y
 
 # Running Ansible to automate the image creation and run
     echo "Running Ansible to automate the image creation and run"
-ansible-playbook -i inventory.ini docker-express.yml
+ansible-playbook -i inventory.ini install_express.yml
 
 # Create a docker image and run docker container
     echo "Create a docker image and run docker container"
-docker build -t express-app
-docker run -p 3000:3000 express-app
+docker build -t express
+docker run -p 3000:3000 express
 
 # Container is running
     echo "Container is running"
