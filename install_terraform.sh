bf05bee1869bbaba1ffe4bced0ff14db87f6b40d
20058225 <20058225@mydbs.ie>
2024-11-07 12:04:19 -0300

    up

--- install_terraform.sh
+++ install_terraform.sh
@@ -6,12 +6,10 @@ sudo apt-get update
 sudo apt-get install -y gnupg software-properties-common curl unzip
 
 # Add HashiCorp GPG key and repo
-#wget https://releases.hashicorp.com/terraform/1.9.7/terraform_1.9.7_linux_amd64.zip
-#unzip terraform_1.9.7_linux_amd64.zip
-#sudo mv terraform /usr/local/bin/
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update && sudo apt install terraform
+wget https://releases.hashicorp.com/terraform/1.9.7/terraform_1.9.7_linux_amd64.zip
+unzip terraform_1.9.7_linux_amd64.zip
+sudo mv terraform /usr/local/bin/
+
 
 # Update package lists
 echo "Updating package lists..."
