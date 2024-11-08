9ec2614af004db2f3da0ab548f9df72fa39ced6e
20058225 <20058225@mydbs.ie>
2024-11-08 10:32:38 -0300

    up ansible pip install

--- install_ansible.sh
+++ install_ansible.sh
@@ -3,13 +3,11 @@ echo "System Preparation in progress..."
   sudo apt update
   sudo apt install software-properties-common
   sudo apt-add-repository --yes --update ppa:ansible/ansible
-  sudo apt install ansible -y
+  sudo apt install -y ansible
 
 # Installing pip
 echo "Installing pip in progress..."
-  curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
-  python3 get-pip.py --user
-  sudo apt-get install python3-pip -y
+  sudo apt-get install -y python3-pip
 
 # Shows the python version installed
 echo "Python Version installed"
@@ -17,9 +15,10 @@ echo "Python Version installed"
 
 # Setting Up the Python Virtual Environment
 echo "Setting Up the Python Virtual Environment in progress..."
-  sudo apt install python3-venv -y
+  sudo apt install -y python3-venv
   python3 -m venv myenv
   source myenv/bin/activate
+  
   pip install ansible
 
 # Verifying Ansible Installation
@@ -28,6 +27,6 @@ echo "Verifying the Ansible Installation..."
 
 # Runing Ansible with an Inventory and Playbook
 echo "Runing Ansible with an Inventory and Playbook in progress..."
-  ansible all -i inventory.ini -m ping host
+  ansible all -i inventory.ini -m ping
   sudo usermod -aG docker useradmin
   ansible-playbook -i inventory.ini install_docker.yml
