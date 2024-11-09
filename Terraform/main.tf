37ea66bc26982daa94cfeaa40130112316b6f4d1
20058225 <20058225@mydbs.ie>
2024-11-09 08:48:11 -0300

    up

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -65,6 +65,28 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  security_rule {
+    name                       = "Express"
+    priority                   = 360
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+  security_rule {
+    name                       = "Jenkins"
+    priority                   = 380
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3000"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 }
 resource "azurerm_public_ip" "public_ip" {
   name                = "appserver-public_ip"
