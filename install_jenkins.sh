006118962a61f29c000fa2c159b438efbd5c02a7
20058225 <20058225@mydbs.ie>
2024-11-09 10:53:51 -0300

    Update install_jenkins.sh

--- install_jenkins.sh
+++ install_jenkins.sh
@@ -1,15 +1,15 @@
 # Updating System
-  echo "Updating system.."
+  echo "@@ Updating system..."
 sudo apt update
 sudo apt upgrade -y
 
 # Install Java and verify
-  echo "Installing Java.."
-sudo apt install fontconfig openjdk-17-jre
+  echo "@@ Installing Java..."
+sudo apt install fontconfig openjdk-17-jre -y
 java -version
 
 # Download and install Jenkins 
-  echo "Adding repository key..."
+  echo "@@ Adding repository key..."
 sudo wget -O /usr/share/keyrings/jenkins-keyring.asc \
   https://pkg.jenkins.io/debian/jenkins.io-2023.key
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
@@ -18,20 +18,20 @@ echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc]" \
 curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
   /usr/share/keyrings/jenkins-keyring.asc > /dev/null
 
-  echo "Updating list file.."
+  echo "@@ Updating list file..."
 echo "deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
   https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
   /etc/apt/sources.list.d/jenkins.list > /dev/null
 
-  echo "Updating and installing Jenkins..."
+  echo "@@ Updating and installing Jenkins..."
 sudo apt-get update
 sudo apt-get install jenkins -y 
 
-  echo "Checking Jenkins version"
+  echo "@@ Checking Jenkins version"
 jenkins --version
 
 # Start Jenkins and Enable Jenkins to start on boot
-  echo "Starting Jenkins.."
+  echo "@@ Starting Jenkins.."
 sudo systemctl start jenkins
 sudo systemctl enable jenkins
 
